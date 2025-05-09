0b20507188ac3dfbb389db36673a149baa381632
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-05-08 22:52:39 -0500

    Remove contact information from OpenApiConfig class, streamlining the OpenAPI configuration without affecting functionality.

--- src/main/java/com/sigrap/config/OpenApiConfig.java
+++ src/main/java/com/sigrap/config/OpenApiConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
@@ -24,9 +23,6 @@ public class OpenApiConfig {
             " product management."
           )
           .version("v1.0.0")
-          .contact(
-            new Contact().name("SIGRAP Team").email("contact@sigrap.com")
-          )
           .license(
             new License()
               .name("MIT License")
